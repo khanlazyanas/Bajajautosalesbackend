3835cd83b7a252ca8e5eddeedd40688d31f5265d
Anas Khan <anaskhan995620@gmail.com>
2025-11-16 10:37:41 +0530

    Correct folder case rename

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app  from "./app.js";
 import dotenv from "dotenv"
-import { connectdatabase } from "./Config/database.js";
+import { connectdatabase } from "./config/database.js";
 
 dotenv.config()
 
